a2f39f83518974679471f17313c865066f868a5c
Ryan Baxley <rynobax@users.noreply.github.com>
2017-08-28 12:08:37 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,7 @@
 console.log('Test.js loaded');
+var workerCount = 0;
+var myWorker = new Worker('ww.js');
+myWorker.onmessage = function(e) {
+  workerCount++;
+  console.log('Worker count: ' + workerCount);
+}
