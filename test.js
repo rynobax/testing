6f16b52a39cd1b88fe6d04a1ff642d0d438c09e3
Ryan Baxley <rynobax@users.noreply.github.com>
2017-08-28 14:33:28 -0400

    Update test.js

--- test.js
+++ test.js
@@ -3,6 +3,7 @@ console.log('Test.js loaded');
 function addButton(name, func) {
   var button = document.createElement('button')
   button.addEventListener ("click", func);
+  button.innerHTML = name;
   document.body.appendChild(button);
 }
 
